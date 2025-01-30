734718eeb8d7d45a1b33182b1443a1fc430f878f
Malek BOUMEDINE <malek.boumedine@gmail.com>
2025-01-31 00:43:29 +0100

    ajout user dans les env

--- prime_assurance/settings.py
+++ prime_assurance/settings.py
@@ -152,6 +152,6 @@ EMAIL_HOST_PASSWORD = 'wosdxtiuxexloijz'
 
 django_heroku.settings(locals())
 
-DJANGO_SUPERUSER_USERNAME = os.environ.get('admin')
-DJANGO_SUPERUSER_EMAIL = os.environ.get('alghom.ia@gmail.com')
-DJANGO_SUPERUSER_PASSWORD = os.environ.get('admin')
+DJANGO_SUPERUSER_USERNAME = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+DJANGO_SUPERUSER_EMAIL = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+DJANGO_SUPERUSER_PASSWORD = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
