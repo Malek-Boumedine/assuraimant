c6dedc252ed1851c7ed18bfe0a36ba3f5f50b51a
Malek BOUMEDINE <malek.boumedine@gmail.com>
2025-01-31 09:10:25 +0100

    ajout script2 super user

--- prime_assurance/create_superuser.py
+++ prime_assurance/create_superuser.py
@@ -1,12 +1,6 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
 
-class Command(BaseCommand):
-    help = 'Create a superuser if it does not exist'
-
-    def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'alghom.ia@gmail.com', 'admin')
-            self.stdout.write(self.style.SUCCESS('Successfully created superuser'))
-        else:
-            self.stdout.write(self.style.WARNING('Superuser already exists'))
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "alghom.ia@gmail.com", "admin")
